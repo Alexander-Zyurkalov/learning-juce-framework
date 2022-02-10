873f3f6342cd419f00654a6bc694fb4a205821ab
Alexandr Zyurkalov <alexandr.zyurkalov@idealo.de>
2022-02-10 21:34:45 +0100

    now we can create an empty GUI application

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,49 @@
 //
 // Created by Alexandr Zyurkalov on 09.02.22.
 //
-#include <iostream>
-#include <juce_core/juce_core.h>
-int main(int argc, char* argv[]){
-    std::cout << "Hello World" << std::endl;
-    juce::ignoreUnused(argc, argv);
-    return 0;
-}
+#include "juce_core/juce_core.h"
+#include "juce_gui_basics/juce_gui_basics.h"
+
+//==============================================================================
+class MainWindowTutorialApplication  : public juce::JUCEApplication
+{
+public:
+    //...
+    const juce::String getApplicationName() override       { return "ProjectInfo::projectName"; }
+    const juce::String getApplicationVersion() override    { return "ProjectInfo::versionString"; }
+    //==============================================================================
+    class MainWindow    : public juce::DocumentWindow
+    {
+    public:
+        MainWindow (juce::String name)  : DocumentWindow (name,
+                                                          juce::Colours::lightgrey,
+                                                          DocumentWindow::allButtons)
+        {
+            centreWithSize (300, 200);
+            setVisible (true);
+        }
+
+        void closeButtonPressed() override
+        {
+            juce::JUCEApplication::getInstance()->systemRequestedQuit();
+        }
+
+    private:
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
+    };
+    void initialise (const juce::String& commandLine) override
+    {
+        mainWindow.reset (new MainWindow (getApplicationName()));
+    }
+
+    void shutdown() override
+    {
+        mainWindow = nullptr;
+    }
+
+private:
+    std::unique_ptr<MainWindow> mainWindow;
+};
+
+
+START_JUCE_APPLICATION (MainWindowTutorialApplication)
